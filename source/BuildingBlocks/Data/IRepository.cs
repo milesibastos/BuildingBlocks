973b25f047b0f8fcb9d14f28c167b75a112fdc59
Antonio Milesi Bastos <antonio@milesibastos.com>
2015-04-13 19:03:12 -0300

    added BuildingBlocks.Data.NHibernate

--- source/BuildingBlocks/Data/IRepository.cs
+++ source/BuildingBlocks/Data/IRepository.cs
@@ -7,6 +7,10 @@ using BuildingBlocks.Domain;
 
 namespace BuildingBlocks.Data
 {
+    public interface IRepository<TEntity> :
+        IDictionary<int, TEntity>
+        where TEntity : Entity<int> { }
+
     public interface IRepository<TKey, TEntity> : 
         IDictionary<TKey, TEntity>
         where TKey : IEquatable<TKey>
