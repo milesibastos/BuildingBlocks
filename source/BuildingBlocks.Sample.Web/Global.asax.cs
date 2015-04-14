13f38d785213bc33304ba01b565a3f85f8c6b4ea
Antonio Milesi Bastos <antonio@milesibastos.com>
2015-04-14 20:13:35 -0300

    add NHibernate.DomainModel

--- source/BuildingBlocks.Sample.Web/Global.asax.cs
+++ source/BuildingBlocks.Sample.Web/Global.asax.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using NHibernate;
+using NHibernate.Cfg;
 
 namespace BuildingBlocks.Sample.Web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static readonly IEnumerable<ISessionFactory> _sessionFactory = new List<ISessionFactory>();
+
+        static MvcApplication()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            var Configuration = new Configuration();
+            string nhConfigPath = HostingEnvironment.MapPath("~/App_Data/hibernate.cfg.xml");
+            if (File.Exists(nhConfigPath))
+            {
+                Configuration.Configure(nhConfigPath);
+            }
+            Configuration.SetDefaultAssembly(typeof(Item).Assembly.FullName)
+                .SetDefaultNamespace(typeof(Item).Namespace)
+                .AddDirectory(new DirectoryInfo(HostingEnvironment.MapPath("~/App_Data/")));
+
+            var _factory = Configuration.BuildSessionFactory();
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
