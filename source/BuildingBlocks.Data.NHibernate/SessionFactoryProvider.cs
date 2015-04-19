13c367fd96ba04cad58f3d8146fda0f08a3a51fb
Antonio Milesi Bastos <antonio@milesibastos.com>
2015-04-19 18:41:10 -0300

    add SessionFactoryProvider, SessionContext

---
+++ source/BuildingBlocks.Data.NHibernate/SessionFactoryProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+
+namespace BuildingBlocks.Data.NHibernate
+{
+    public abstract class SessionFactoryProvider : IEnumerable<ISessionFactory>
+    {
+        private static IEnumerable<ISessionFactory> _sessionFactory;
+
+        public abstract IEnumerable<ISessionFactory> BuildSessionFactory();
+
+        public IEnumerator<ISessionFactory> GetEnumerator()
+        {
+            if (_sessionFactory == null)
+                _sessionFactory = BuildSessionFactory();
+
+            return _sessionFactory.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return _sessionFactory.GetEnumerator();
+        }
+    }
+}
